67832e34e84993eaf3a2b394ca6d4bc4be90da0a
Julien <julien@twenty-six-medias.com>
2014-03-14 02:19:51 -0300

    formjs component + nested components example

--- projects/origin/components/account/client/registration/form.js
+++ projects/origin/components/account/client/registration/form.js
@@ -1,39 +1,14 @@
-window.meanEngine.get('app').directive('accountLoginForm', function() {
+window.meanEngine.get('app').directive('accountLoginForm', function($compile) {
 	var component = function(scope, element, attrs) {
-		var form = [{
-			name:			"email",
-			type:			"varchar",
-			label:			"Email",
-			required:		true
-		},{
-			name:			"password",
-			type:			"varchar",
-			label:			"Password",
-			required:		false
-		}];
-		
-		$(function() {	// We make sure jQuery is loaded before we generate the form.
-			var jform = new formjs($(element), ['bootstrap']).build({	// We are generating the form in the #form container
-				form:		form,			// The form data
-				submit:		$("#submit"),	// The submit button
-				onSubmit:	function(data, jform) {	// Executed when the entire form validates.
-					// We use the Bootstrap plugin, telling it to reset the previous error display
-					jform.bootstrap.resetErrors();
-					// Output
-					alert(JSON.stringify(data,null,4));
-				},
-				onError:	function(data, jform) {	// Executed when at least one question didn't validate.
-					// We use the Bootstrap plugin, telling it to visually show us the errors using the bootstrap theme.
-					jform.bootstrap.showErrors();
-					// You can manage your own error display. "data" is an array containing the list of fields that did not validate.
-					// We won't do that in that example.
-					alert("You have errors on the form.");
-				}
-			});
-		});
+		scope.settings = {
+			container:		element,
+			submitButton:	element.find('[data-id="submit"]')
+		};
+		$compile(element.contents())(scope);
 	}
 
 	return {
-		link: component
+		link: 			component,
+		templateUrl:	'components/account/client/registration/form.html'
 	};
 });
